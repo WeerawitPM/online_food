a5a4a95f591df3c45e0a18d36e78dadbf3713943
WeerawitPM <63030079@kmitl.ac.th>
2023-11-04 13:56:00 +0700

    update: order_status

--- customer/order_status.php
+++ customer/order_status.php
@@ -10,8 +10,8 @@ if (isset($_POST["food_id"])) {
 
     $sql = "UPDATE food_order SET status = '$status' WHERE food_id ='$food_id'";
     $result = $conn->query($sql);
-    if($result === true) {
-        echo"<script>alert('เปลี่ยนสถานะของอาหารเสร็จสิ้น');</script>";
+    if ($result === true) {
+        echo "<script>alert('เปลี่ยนสถานะของอาหารเสร็จสิ้น');</script>";
     }
 }
 ?>
@@ -22,110 +22,144 @@ if (isset($_POST["food_id"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>สถานะคำสั่งซื้อ</title>
 </head>
 
 <body>
     <?php include("navbar.php"); ?>
     <h1>รายการอาหารที่สั่ง</h1>
-    <?php
-    $customer_id = $_SESSION["id"];
+    <div class="d-flex flex-row justify-content-center align-items-start flex-wrap">
+        <?php
+        $customer_id = $_SESSION["id"];
 
-    $status1 = 'รอคนขับ';
-    $sql = "SELECT * FROM food_order WHERE customer_id = '$customer_id' AND status = '$status1'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $food_id = $row["food_id"];
-            $count = $row["count"];
-            $total_price = $row["total_price"];
+        $status1 = 'รอคนขับ';
+        $sql = "SELECT * FROM food_order WHERE customer_id = '$customer_id' AND status = '$status1'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $food_id = $row["food_id"];
+                $count = $row["count"];
+                $total_price = $row["total_price"];
 
-            $sql = "SELECT * FROM food WHERE id = '$food_id'";
-            $result2 = $conn->query($sql);
-            $row2 = $result2->fetch_assoc();
-            $name = $row2["name"];
-            $image = $row2["image"];
-            
-            echo "
-            <img src='../restaurant/$image'>
-            <p>ชื่ออาหาร: $name</p>
-            <p>จำนวน: $count</p>
-            <p>ราคา: $total_price</p>
-            <p>สถานะ: $status1</p>
-            <hr>
-            ";
+                $sql = "SELECT * FROM food WHERE id = '$food_id'";
+                $result2 = $conn->query($sql);
+                $row2 = $result2->fetch_assoc();
+                $name = $row2["name"];
+                $image = $row2["image"];
+
+                echo "
+                <div class='card m-2' style='width: 18rem;'>
+                    <img src='../restaurant/$image' class='card-img-top' alt='...' width='100px' >
+                    <div class='card-body'>
+                        <h2 class='card-title'>$name</h2>
+                        <p class='card-text'>จำนวน : $count</p>
+                        <h3 class='card-text'>฿ $total_price</h3>
+                    </div>
+                    <center>
+                        <div class='card-footer'>
+                            สถานะ: $status1
+                        </div>
+                    </center>
+                </div>
+                ";
+            }
         }
-    }
 
-    $status2 = 'รอร้านค้าทำอาหาร';
-    $sql = "SELECT * FROM food_order WHERE customer_id = '$customer_id' AND status = '$status2'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $food_id = $row["food_id"];
-            $count = $row["count"];
-            $total_price = $row["total_price"];
-            $sql = "SELECT * FROM food WHERE id = '$food_id'";
-            $result2 = $conn->query($sql);
-            $row2 = $result2->fetch_assoc();
-            $name = $row2["name"];
-            echo "
-            <p>ชื่ออาหาร: $name</p>
-            <p>จำนวน: $count</p>
-            <p>ราคา: $total_price</p>
-            <p>สถานะ: $status1</p>
-            <hr>
+        $status2 = 'รอร้านค้าทำอาหาร';
+        $sql = "SELECT * FROM food_order WHERE customer_id = '$customer_id' AND status = '$status2'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $food_id = $row["food_id"];
+                $count = $row["count"];
+                $total_price = $row["total_price"];
+                $sql = "SELECT * FROM food WHERE id = '$food_id'";
+                $result2 = $conn->query($sql);
+                $row2 = $result2->fetch_assoc();
+                $name = $row2["name"];
+                echo "
+                <div class='card m-2' style='width: 18rem;'>
+                    <img src='../restaurant/$image' class='card-img-top' alt='...' width='100px' >
+                    <div class='card-body'>
+                        <h2 class='card-title'>$name</h2>
+                        <p class='card-text'>จำนวน : $count</p>
+                        <h3 class='card-text'>฿ $total_price</h3>
+                    </div>
+                    <center>
+                        <div class='card-footer'>
+                            สถานะ: $status2
+                        </div>
+                    </center>
+                </div>
             ";
+            }
         }
-    }
 
-    $status3 = 'กำลังส่ง';
-    $sql = "SELECT * FROM food_order WHERE customer_id = '$customer_id' AND status = '$status3'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $food_id = $row["food_id"];
-            $count = $row["count"];
-            $total_price = $row["total_price"];
-            $sql = "SELECT * FROM food WHERE id = '$food_id'";
-            $result2 = $conn->query($sql);
-            $row2 = $result2->fetch_assoc();
-            $name = $row2["name"];
-            echo "
-            <p>ชื่ออาหาร: $name</p>
-            <p>จำนวน: $count</p>
-            <p>ราคา: $total_price</p>
-            <p>สถานะ: $status1</p>
-            <hr>
+        $status3 = 'กำลังส่ง';
+        $sql = "SELECT * FROM food_order WHERE customer_id = '$customer_id' AND status = '$status3'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $food_id = $row["food_id"];
+                $count = $row["count"];
+                $total_price = $row["total_price"];
+                $sql = "SELECT * FROM food WHERE id = '$food_id'";
+                $result2 = $conn->query($sql);
+                $row2 = $result2->fetch_assoc();
+                $name = $row2["name"];
+                echo "
+                <div class='card m-2' style='width: 18rem;'>
+                    <img src='../restaurant/$image' class='card-img-top' alt='...' width='100px' >
+                    <div class='card-body'>
+                        <h2 class='card-title'>$name</h2>
+                        <p class='card-text'>จำนวน : $count</p>
+                        <h3 class='card-text'>฿ $total_price</h3>
+                    </div>
+                    <center>
+                        <div class='card-footer'>
+                            สถานะ: $status3
+                        </div>
+                    </center>
+                </div>
             ";
+            }
         }
-    }
 
-    $status4 = 'ส่งอาหารแล้ว';
-    $sql = "SELECT * FROM food_order WHERE customer_id = '$customer_id' AND status = '$status4'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $food_id = $row["food_id"];
-            $count = $row["count"];
-            $total_price = $row["total_price"];
-            $sql = "SELECT * FROM food WHERE id = '$food_id'";
-            $result2 = $conn->query($sql);
-            $row2 = $result2->fetch_assoc();
-            $name = $row2["name"];
-            echo "
-            <p>ชื่ออาหาร: $name</p>
-            <p>จำนวน: $count</p>
-            <p>ราคา: $total_price</p>
-            <p>สถานะ: $status1</p>
-            <form action='' method='post'>
-                <button type='submit' value='$food_id' name='food_id'>กดยืนยันรับอาหาร</button>
-            </form>
-            <hr>
+        $status4 = 'ส่งอาหารแล้ว';
+        $sql = "SELECT * FROM food_order WHERE customer_id = '$customer_id' AND status = '$status4'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $food_id = $row["food_id"];
+                $count = $row["count"];
+                $total_price = $row["total_price"];
+                $sql = "SELECT * FROM food WHERE id = '$food_id'";
+                $result2 = $conn->query($sql);
+                $row2 = $result2->fetch_assoc();
+                $name = $row2["name"];
+                echo "
+                <div class='card m-2' style='width: 18rem;'>
+                    <img src='../restaurant/$image' class='card-img-top' alt='...' width='100px' >
+                    <div class='card-body'>
+                        <h2 class='card-title'>$name</h2>
+                        <p class='card-text'>จำนวน : $count</p>
+                        <h3 class='card-text'>฿ $total_price</h3>
+                    </div>
+                    <center>
+                        <div class='card-footer'>
+                            <p>สถานะ: $status4</p>
+                            <form action='' method='post'>
+                                <button type='submit' value='$food_id' name='food_id' class='btn btn-primary'>กดยืนยันรับอาหาร</button>
+                            </form>
+                        </div>
+                    </center>
+                </div>
             ";
+            }
         }
-    }
-    ?>
+        ?>
+    </div>
 </body>
 
 </html>
